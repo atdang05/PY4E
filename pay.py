a6527c50a071849a2d3d9dd955ab49302f96a68b
atdang05 <atdang05@gmail.com>
2024-10-05 18:56:36 +0700

    Exercise 1 Lesson 4: Conditional execution

--- pay.py
+++ pay.py
@@ -1,3 +1,9 @@
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate: "))
-print("Pay:", hours * rate)
+
+if hours <= 40:
+    pay = hours * rate
+else:
+    pay = 40 * rate + (hours - 40) * rate * 1.5
+
+print("Pay:", pay)
